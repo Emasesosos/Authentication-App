30131a1bc825719243e377ea5f3996a2a1c00500
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2021-01-04 10:02:45 -0600

    Estilos OptionsProfile

--- authentication-app/src/components/Home/OptionsProfile.js
+++ authentication-app/src/components/Home/OptionsProfile.js
@@ -6,10 +6,16 @@ export const OptionsProfile = () => {
 
         <div className="optionsProfile__container">
             <div className="optionsProfile__myProfile">
+                <div className="optionsProfile__account-circle">
+                    <i className="material-icons md-dark md-inactive" style={{color: "#4F4F4F", fontSize: "16.67px"}}>account_circle</i>
+                </div>
                 <p>My Profile</p>
             </div>
             <hr/>
             <div className="optionsProfile__logout">
+                <div className="optionsProfile__exit-to-app">
+                    <i className="material-icons md-dark md-inactive" style={{color: "red", fontSize: "15px"}}>exit_to_app</i>
+                </div>
                 <p>Logout</p>
             </div>
         </div>
