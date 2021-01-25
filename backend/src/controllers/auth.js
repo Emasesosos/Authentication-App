4e0e7f7af17527e63dce15c9ef5291efa48e5218
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2021-01-24 20:39:56 -0600

    Autenticación con Google

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -117,8 +117,126 @@ const revalidarToken = async(req, res = response) => {
 
 };
 
+// Obtener Usuarios
+const getUsuarioSocialNet = async(req, res = response) => {
+
+    const { id: email } = req.params;
+    const usuario = await Usuario.find({ email });
+
+    if (usuario.length > 0) {
+        res.status(201).json({
+            ok: true
+        });
+    } else {
+        res.status(201).json({
+            ok: false
+        });
+    }
+
+};
+
+// Crear Usuario Social Networks
+const crearUsuarioSocialNet = async(req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        let usuario = await Usuario.findOne({ email: email });
+        // console.log(usuario);
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Un usuario existe con ese correo'
+            });
+        }
+
+        usuario = new Usuario(req.body);
+
+        // Valores por default del Usuario
+        usuario.imageUrl = 'https://res.cloudinary.com/emasesosos/image/upload/v1610838658/addImage_siw6ys.png';
+        usuario.name = 'Edit Name';
+        usuario.bio = 'Edit Bio';
+        usuario.phone = 'Edit Phone';
+
+        // Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
+
+        await usuario.save();
+
+        // Generar nuestro JWT
+        const token = await generarJWT(usuario.id, usuario.name);
+
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+            token
+        });
+
+    } catch (error) {
+        // console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el Administrador'
+        });
+    }
+
+};
+
+// Login Usuario Social Networks
+const loginUsuarioSocialNet = async(req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        const usuario = await Usuario.findOne({ email: email });
+        // console.log(usuario);
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario no existe con ese email'
+            });
+        }
+
+        // Confirmar los passwords
+        const validPassword = bcrypt.compareSync(password, usuario.password);
+
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Password incorrecto'
+            });
+        }
+
+        // Generar nuestro JWT
+        const token = await generarJWT(usuario.id, usuario.name);
+
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+            token
+        });
+
+
+    } catch (error) {
+        // console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el Administrador'
+        });
+    }
+
+};
+
 module.exports = {
     crearUsuario,
     loginUsuario,
-    revalidarToken
+    revalidarToken,
+    getUsuarioSocialNet,
+    crearUsuarioSocialNet,
+    loginUsuarioSocialNet
 };
--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -6,7 +6,14 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('./../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
-const { crearUsuario, loginUsuario, revalidarToken } = require('./../controllers/auth');
+const {
+    crearUsuario,
+    loginUsuario,
+    revalidarToken,
+    getUsuarioSocialNet,
+    crearUsuarioSocialNet,
+    loginUsuarioSocialNet
+} = require('./../controllers/auth');
 
 const router = Router();
 
@@ -29,6 +36,30 @@ router.post(
         validarCampos
     ],
     loginUsuario
+);
+
+// Obtener Usuario Social Network
+router.get('/searchUserSocialNet/:id', getUsuarioSocialNet);
+
+// Crear Usuario Social Network
+router.post(
+    '/newSocialNet', [ // Middlewares
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+        validarCampos
+
+    ],
+    crearUsuarioSocialNet
+);
+
+// Login Usuario Social Network
+router.post(
+    '/loginSocialNet', [ // Middlewares
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+        validarCampos
+    ],
+    loginUsuarioSocialNet
 );
 
 // Revalidar Token para Usuario
