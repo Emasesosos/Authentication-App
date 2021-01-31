5c2fcb82e3c877552260bcb5ae09bb60de136d1b
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2021-01-30 23:46:32 -0600

    Config BD Producción

--- backend/src/database/config.js
+++ backend/src/database/config.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
-        await mongoose.connect(process.env.DB_MONGO_LOCAL, {
+        await mongoose.connect(process.env.DB_CNN, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
         });
-        console.log('DB Online on Port: ', process.env.DB_MONGO_LOCAL);
+        console.log('DB Online on Port: ', process.env.DB_CNN);
     } catch (error) {
         console.log(error);
         throw new Error('Error a la hora de inicializar la BD');
